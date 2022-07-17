2139f9a4816e5becaa8f58bff298b2cd3586fc4f
Enas Sello <enas.awad169$gmail.com>
2022-07-17 20:41:31 +0200

    Footer componants

--- FrontEnd/src/Components/Footer/Footer.jsx
+++ FrontEnd/src/Components/Footer/Footer.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
-
+import React from 'react';
+import { Row } from 'react-bootstrap';
+import { Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/amazon_logo.png';
 const Footer = () => {
   return (
-    <div className='text-center'>Footer</div>
-  )
-}
+    <Row className="bg-dark d-flex items-start justify-between text-start">
+      <Col>
+        <Link to="/">
+          <img
+            style={{ width: '120px',marginLeft:'5rem' }}
+            className="p-3"
+            src={logo}
+            alt={logo}
+          />
+        </Link>
+      </Col>
+    </Row>
+  );
+};
 
-export default Footer
+export default Footer;
