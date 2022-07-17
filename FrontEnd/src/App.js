e0909d5b8766f0ad56bcf3751beb30ebd1a8a48c
Diaa Ahmed <diaa.aboelela@gmail.com>
2022-07-17 20:27:20 +0200

    cart-page-modified

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,27 +1,32 @@
-import { Container } from 'react-bootstrap';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Footer from './Components/Footer/Footer';
-import Header from './Components/Header/Header';
-import './index.css';
-import ProHome from './pages/ProHome';
-import ProductDetails from './pages/ProductDetails';
+import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Footer from "./Components/Footer/Footer";
+import Header from "./Components/Header/Header";
+import CartPage from "./pages/Cart/CartPage.jsx";
+import "./index.css";
+import ProHome from "./pages/ProHome";
+import ProductDetails from "./pages/ProductDetails";
 function App() {
-  return (
-    <Router>
-      <div className="d-flex flex-column main">
-        <Header />
-        <main>
-          <Container className="mt-3">
-            <Routes>
-              <Route path="/product/:id" element={<ProductDetails />} />
-              <Route path="/" element={<ProHome />} />
-            </Routes>
-          </Container>
-        </main>
-        <Footer />
-      </div>
-    </Router>
-  );
+    return (
+        <Router>
+            <div className="d-flex flex-column main">
+                <Header />
+                <main>
+                    <Container className="mt-3">
+                        <Routes>
+                            <Route path="/CartPage" element={<CartPage />} />
+                            <Route
+                                path="/product/:id"
+                                element={<ProductDetails />}
+                            />
+                            <Route path="/" element={<ProHome />} />
+                        </Routes>
+                    </Container>
+                </main>
+                <Footer />
+            </div>
+        </Router>
+    );
 }
 
 export default App;
--- FrontEnd/src/pages/CartPage.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class CartPage extends Component {
-    render() {
-        return <div>CartPage</div>;
-    }
-}
-
-export default CartPage;
