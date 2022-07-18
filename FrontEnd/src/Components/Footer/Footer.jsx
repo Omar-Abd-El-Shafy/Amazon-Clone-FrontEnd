7ea47a6c04790dac8033c8bcfc46594802b3a3b7
Enas Sello <enas.awad169$gmail.com>
2022-07-18 21:23:41 +0200

    fixed footer

--- FrontEnd/src/Components/Footer/Footer.jsx
+++ FrontEnd/src/Components/Footer/Footer.jsx
@@ -1,22 +1,23 @@
 import React from 'react';
-import { Row } from 'react-bootstrap';
-import { Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import logo from '../../assets/amazon_logo.png';
 const Footer = () => {
   return (
-    <Row className="bg-dark d-flex items-start justify-between text-start">
-      <Col>
+    <div
+      style={{ maxWidth: '100%' }}
+      className="bg-dark d-flex items-start justify-between"
+    >
+      <div>
         <Link to="/">
           <img
-            style={{ width: '120px',marginLeft:'5rem' }}
+            style={{ width: '120px', marginLeft: '5rem' }}
             className="p-3"
             src={logo}
             alt={logo}
           />
         </Link>
-      </Col>
-    </Row>
+      </div>
+    </div>
   );
 };
 
