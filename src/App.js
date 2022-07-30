e3ccfa315d754517e4d0ffe65d3c5bfd97f73cf5
Diaa Ahmed <diaa.aboelela@gmail.com>
2022-07-30 17:46:20 +0200

    cartSlice-mod

--- src/App.js
+++ src/App.js
@@ -10,52 +10,31 @@ import { Container } from "react-bootstrap";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./index.css";
 import { useState } from "react";
-import ForgotPassword from "./pages/ForgotPassword";
-import PasswordReset from "./pages/PasswordReset";
 import SideBar from "./Components/SideBar/SideBar";
 import Dashboard from "./pages/Dashboard";
 import DashProducts from "./Components/Dashboard/DashProducts";
 import DashMain from "./Components/Dashboard/DashMain";
 
 function App() {
-  const [showNav, setShowNav] = useState(true);
-  return (
-    <Router>
-      <div className="d-flex flex-column main">
-        {showNav && (
-          <nav>
-            <Header />
-          </nav>
-        )}
-        <main>
-          <Container className="mt-3">
-            <Routes>
-              <Route path="/CartPage" element={<CartPage />} />
-              <Route path="/product/:id" element={<ProductDetails />} />
+    const [showNav, setShowNav] = useState(true);
+    return (
+        <Router>
+            <div className="d-flex flex-column main">
+                {showNav && (
+                    <div>
+                        <Header />
+                    </div>
+                )}
+                {showNav && <SideBar />}
+                <main>
+                    <Container className="mt-3">
+                        <Routes>
+                            <Route path="/CartPage" element={<CartPage />} />
+                            <Route
+                                path="/product/:id"
+                                element={<ProductDetails />}
+                            />
 
-              <Route path="/" element={<ProHome />} />
-              <Route path="/login" element={<Login funcNav={setShowNav} />} />
-              <Route path="/signup" element={<Signup funcNav={setShowNav} />} />
-              <Route
-                path="/forgot-password"
-                element={<ForgotPassword funcNav={setShowNav} />}
-              />
-              <Route
-                path="/password-reset/:id/:token"
-                element={<PasswordReset />}
-              />
-            </Routes>
-          </Container>
-        </main>
-        {showNav && (
-          <footer>
-            <BackTop />
-            <Footer />
-          </footer>
-        )}
-      </div>
-    </Router>
-  );
                             <Route path="/" element={<ProHome />} />
                             <Route
                                 path="/login"
--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -1,15 +1,15 @@
-import { configureStore } from '@reduxjs/toolkit';
-import cartReducer from './cartRedux';
-import ProductsReducer, { productFetch } from './ProductSlice.jsx';
-import { prodactsApi } from './prodactsApi';
+import { configureStore } from "@reduxjs/toolkit";
+import cartReducer from "./cartSlice";
+import ProductsReducer, { productFetch } from "./ProductSlice.jsx";
+import { prodactsApi } from "./prodactsApi";
 const store = configureStore({
-  reducer: {
-    cart: cartReducer,
-    prodacts: ProductsReducer,
-    [prodactsApi.reducerPath]: prodactsApi.reducer,
-  },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(prodactsApi.middleware),
+    reducer: {
+        cart: cartReducer,
+        prodacts: ProductsReducer,
+        [prodactsApi.reducerPath]: prodactsApi.reducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(prodactsApi.middleware),
 });
 store.dispatch(productFetch());
 export default store;
