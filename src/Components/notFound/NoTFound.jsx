0c4da602f80adfd93852470ecb12d7e172ed51f0
Diaa Ahmed <diaa.aboelela@gmail.com>
2022-08-03 13:54:03 +0200

    notfound-page-mod

--- src/Components/notFound/NoTFound.jsx
+++ src/Components/notFound/NoTFound.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import Container from 'react-bootstrap/esm/Container'
+import React from "react";
+import Container from "react-bootstrap/esm/Container";
+import { Link } from "react-router-dom";
+import NotFoundImage from "../../assets/imgs/logo/Not-Found.png";
 const NoTFound = () => {
     return (
-      <Container>
-            
-          <h1>404</h1>
-          <h1>Page Not Found</h1>
-      </Container>
-  )
-}
+        <Container>
+            <Link to="/" className="d-block text-center">
+                <img src={NotFoundImage} alt="notFound" className="mt-5 w-50" />
+            </Link>
+        </Container>
+    );
+};
 
-export default NoTFound
+export default NoTFound;
