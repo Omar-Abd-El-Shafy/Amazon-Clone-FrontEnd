36fc90f8e868a1fd3af4570ebb87d1cd4289c4f3
enas sello <enas.awad169@gmail.com>
2022-08-07 18:53:22 +0200

    sidebar

--- src/Components/SideBar/SidebarList.jsx
+++ src/Components/SideBar/SidebarList.jsx
@@ -1,59 +1,62 @@
-import Nav from "react-bootstrap/Nav";
-import { IoIosArrowForward } from "react-icons/io";
+import { Row } from 'react-bootstrap';
+import Nav from 'react-bootstrap/Nav';
+import { IoIosArrowForward } from 'react-icons/io';
+import { useGetAllCategoriesQuery } from '../../Redux/prodactsApi';
 function SidebarList() {
-    return (
-        <Nav defaultActiveKey="/home" className="flex-column">
-            <h5>Shop By Department</h5>
+  const {
+    data: category,
+    isError: error,
+    isLoading: loading,
+  } = useGetAllCategoriesQuery();
+  return (
+    <Nav defaultActiveKey="/home" className="flex-column">
+      <h5>Shop By Department</h5>
+      <Nav.Link
+        href="/"
+        className="sidebar-navs d-flex justify-content-between"
+      >
+        Active
+        <IoIosArrowForward />
+      </Nav.Link>
+      <Nav.Link
+        href="/"
+        eventKey="link-1"
+        className="sidebar-navs d-flex justify-content-between"
+      >
+        Link
+        <IoIosArrowForward />
+      </Nav.Link>
+      <Nav.Link
+        eventKey="link-2"
+        className="sidebar-navs d-flex justify-content-between"
+      >
+        Link
+        <IoIosArrowForward />
+      </Nav.Link>
+      <div>
+        <hr />
+      </div>
+      <h5>Shop By Category</h5>
+      {loading ? (
+        <div>Category ....</div>
+      ) : error ? (
+        { error }
+      ) : (
+        <Row className="mt-4" style={{ marginRight: '0', marginLeft: ' 0' }}>
+          {category.map((item) => (
             <Nav.Link
-                href="/"
-                className="sidebar-navs d-flex justify-content-between"
+              href={`/products/category/${item}`}
+              className="sidebar-navs d-flex justify-content-between"
             >
-                Active
-                <IoIosArrowForward />
+              {item}
+              <IoIosArrowForward />
             </Nav.Link>
-            <Nav.Link
-                href="/"
-                eventKey="link-1"
-                className="sidebar-navs d-flex justify-content-between"
-            >
-                Link
-                <IoIosArrowForward />
-            </Nav.Link>
-            <Nav.Link
-                eventKey="link-2"
-                className="sidebar-navs d-flex justify-content-between"
-            >
-                Link
-                <IoIosArrowForward />
-            </Nav.Link>
-            <div>
-                <hr />
-            </div>
-            <h5>Shop By Category</h5>
-            <Nav.Link
-                href="/"
-                className="sidebar-navs d-flex justify-content-between"
-            >
-                Active
-                <IoIosArrowForward />
-            </Nav.Link>
-            <Nav.Link
-                href="/"
-                eventKey="link-1"
-                className="sidebar-navs d-flex justify-content-between"
-            >
-                Link
-                <IoIosArrowForward />
-            </Nav.Link>
-            <Nav.Link
-                eventKey="link-2"
-                className="sidebar-navs d-flex justify-content-between"
-            >
-                Link
-                <IoIosArrowForward />
-            </Nav.Link>
-        </Nav>
-    );
+          ))}
+        </Row>
+      )}
+      
+    </Nav>
+  );
 }
 
 export default SidebarList;
