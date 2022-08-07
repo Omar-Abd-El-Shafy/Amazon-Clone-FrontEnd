a5b3f16658d8c2a3e0ed7519fe99960b2dad70c1
Diaa Ahmed <diaa.aboelela@gmail.com>
2022-08-07 21:07:23 +0200

    passwordToggle-signIn-and-signUp

--- src/App.js
+++ src/App.js
@@ -1,38 +1,38 @@
 //admin
-import Dashboard from './pages/Dashboard';
-import DashProducts from './Components/Dashboard/DashProducts';
-import DashMain from './Components/Dashboard/DashMain';
+import Dashboard from "./pages/Dashboard";
+import DashProducts from "./Components/Dashboard/DashProducts";
+import DashMain from "./Components/Dashboard/DashMain";
 //style
-import { Container } from 'react-bootstrap';
-import './index.css';
-import 'react-toastify/dist/ReactToastify.css';
-import 'aos/dist/aos.css';
+import { Container } from "react-bootstrap";
+import "./index.css";
+import "react-toastify/dist/ReactToastify.css";
+import "aos/dist/aos.css";
 //depend
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import React, { useState, useEffect, useCallback } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { ToastContainer } from 'react-toastify';
-import { Slide, Zoom, Flip, Bounce } from 'react-toastify';
-import { userSliceActions } from '../src/Redux/userSlice';
-import AOS from 'aos';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { useState, useEffect, useCallback } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { ToastContainer } from "react-toastify";
+import { Slide, Zoom, Flip, Bounce } from "react-toastify";
+import { userSliceActions } from "../src/Redux/userSlice";
+import AOS from "aos";
 //componants
-import Header from './Components/Header/Header';
-import Footer from './Components/Footer/Footer';
-import SideBar from './Components/SideBar/SideBar';
-import BackTop from './Components/backTop/BackTop';
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
+import SideBar from "./Components/SideBar/SideBar";
+import BackTop from "./Components/backTop/BackTop";
 //pages
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import EditProfileForm from './Components/EditProfileForm/EditProfileForm';
-import ForgotPassword from './pages/ForgotPassword';
-import PasswordReset from './pages/PasswordReset';
-import Home from './pages/Home';
-import CategoryPage from './pages/CategoryPage';
-import ProductDetails from './pages/ProductDetails';
-import NoTFound from './Components/notFound/NoTFound';
-import About from './pages/About';
-import CartPage from './pages/CartPage';
-import Checkout from './pages/Checkout';
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import EditProfileForm from "./Components/EditProfileForm/EditProfileForm";
+import ForgotPassword from "./pages/ForgotPassword";
+import PasswordReset from "./pages/PasswordReset";
+import Home from "./pages/Home";
+import CategoryPage from "./pages/CategoryPage";
+import ProductDetails from "./pages/ProductDetails";
+import NoTFound from "./Components/notFound/NoTFound";
+import About from "./pages/About";
+import CartPage from "./pages/CartPage";
+import Checkout from "./pages/Checkout";
 
 // import AddProduct from "./pages/AddProduct";
 // import tokenExpirationDate from "../src/pages/Login";
@@ -40,112 +40,136 @@ import Checkout from './pages/Checkout';
 // let logoutTimer;
 
 function App() {
-  const dispatch = useDispatch();
-  const loggedInUser = useSelector((state) => state.user.loggedInUser);
-  // const [tokenExpirationDatee, setTokenExpirationDatee] = useState();
-  // setTokenExpirationDatee(tokenExpirationDate);
-  useEffect(() => {
-    AOS.init();
-    AOS.refresh();
+    const dispatch = useDispatch();
+    const loggedInUser = useSelector((state) => state.user.loggedInUser);
+    // const [tokenExpirationDatee, setTokenExpirationDatee] = useState();
+    // setTokenExpirationDatee(tokenExpirationDate);
+    useEffect(() => {
+        AOS.init();
+        AOS.refresh();
 
-    const storedData = JSON.parse(localStorage.getItem('userData'));
-    if (
-      storedData?.userId &&
-      storedData?.token &&
-      new Date(storedData.expiration) > new Date()
-    ) {
-      dispatch(
-        userSliceActions.setUser({
-          id: storedData.userId,
-          userName: storedData.name,
-          userToken: storedData.token,
-        })
-      );
-    }
-  }, [
-    dispatch,
-    loggedInUser?.id,
-    loggedInUser?.userName,
-    loggedInUser?.userToken,
-  ]);
+        const storedData = JSON.parse(localStorage.getItem("userData"));
+        if (
+            storedData?.userId &&
+            storedData?.token &&
+            new Date(storedData.expiration) > new Date()
+        ) {
+            dispatch(
+                userSliceActions.setUser({
+                    id: storedData.userId,
+                    userName: storedData.name,
+                    userToken: storedData.token,
+                })
+            );
+        }
+    }, [
+        dispatch,
+        loggedInUser?.id,
+        loggedInUser?.userName,
+        loggedInUser?.userToken,
+    ]);
 
-  // useEffect(() => {
-  //   if (loggedInUser?.userToken && tokenExpirationDate) {
-  //     const remainingTime =
-  //       tokenExpirationDate.getTime() - new Date().getTime();
-  //     logoutTimer = setTimeout(
-  //       dispatch(userSliceActions.logout()),
-  //       remainingTime
-  //     );
-  //   } else {
-  //     clearTimeout(logoutTimer);
-  //   }
-  // }, [dispatch, loggedInUser?.userToken]);
+    // useEffect(() => {
+    //   if (loggedInUser?.userToken && tokenExpirationDate) {
+    //     const remainingTime =
+    //       tokenExpirationDate.getTime() - new Date().getTime();
+    //     logoutTimer = setTimeout(
+    //       dispatch(userSliceActions.logout()),
+    //       remainingTime
+    //     );
+    //   } else {
+    //     clearTimeout(logoutTimer);
+    //   }
+    // }, [dispatch, loggedInUser?.userToken]);
 
-  // AOS.init();
-  // AOS.refresh();
+    // AOS.init();
+    // AOS.refresh();
 
-  // userId: loggedInUser?.id,
-  // token: loggedInUser?.userToken,
+    // userId: loggedInUser?.id,
+    // token: loggedInUser?.userToken,
 
-  const [showNav, setShowNav] = useState(true);
-  return (
-    <Router>
-      <ToastContainer
-        autoClose="1000"
-        theme="colored"
-        position="bottom-left"
-        transition={Flip}
-      />
-      <div className="d-flex flex-column main">
-        {showNav && (
-          <div>
-            <Header />
-          </div>
-        )}
-        {showNav && <SideBar />}
-        <main>
-          <Container fluid className="mt-5">
-            <Routes>
-              <Route exact path="/" element={<Home funcNav={setShowNav} />} />
-              <Route path="/login" element={<Login funcNav={setShowNav} />} />
-              <Route path="/signup" element={<Signup funcNav={setShowNav} />} />
-              <Route path="/profile" element={<EditProfileForm />} />
-              <Route
-                path="/forgot-password"
-                element={<ForgotPassword funcNav={setShowNav} />}
-              />
-              <Route
-                path="/password-reset/:id/:token"
-                element={<PasswordReset />}
-              />
-              <Route path="/products/category/:id" element={<CategoryPage />} />
-              <Route path="/product/:id" element={<ProductDetails />} />
-              <Route path="/CartPage" element={<CartPage />} />
-              <Route path="/Checkout" element={<Checkout />} />
-              <Route path="/About" element={<About />} />
-              //////////////////////////Admin///////////////////////
-              <Route
-                path="/dashboard"
-                element={<Dashboard funcNav={setShowNav} />}
-              >
-                <Route index element={<DashMain />} />
-                <Route path="DashProducts" element={<DashProducts />} />
-                <Route path="DashMain" element={<DashMain />} />
-              </Route>
-              {/* <Route path="/addProduct" element={<AddProduct />} /> */}
-              <Route path="*" element={<NoTFound />} />
-            </Routes>
-          </Container>
-        </main>
-        {showNav && (
-          <footer>
-            <BackTop />
-            <Footer />
-          </footer>
-        )}
-      </div>
-    </Router>
-  );
+    const [showNav, setShowNav] = useState(true);
+    return (
+        <Router>
+            <ToastContainer
+                autoClose="1000"
+                theme="colored"
+                position="bottom-left"
+                transition={Flip}
+            />
+            <div className="d-flex flex-column main">
+                {showNav && (
+                    <div>
+                        <Header />
+                    </div>
+                )}
+                {showNav && <SideBar />}
+                <main>
+                    <Container fluid className="mt-5">
+                        <Routes>
+                            <Route
+                                exact
+                                path="/"
+                                element={<Home funcNav={setShowNav} />}
+                            />
+                            <Route
+                                path="/login"
+                                element={<Login funcNav={setShowNav} />}
+                            />
+                            <Route
+                                path="/signup"
+                                element={<Signup funcNav={setShowNav} />}
+                            />
+                            <Route
+                                path="/profile"
+                                element={<EditProfileForm />}
+                            />
+                            <Route
+                                path="/forgot-password"
+                                element={
+                                    <ForgotPassword funcNav={setShowNav} />
+                                }
+                            />
+                            <Route
+                                path="/password-reset/:id/:token"
+                                element={<PasswordReset />}
+                            />
+                            <Route
+                                path="/products/category/:id"
+                                element={<CategoryPage />}
+                            />
+                            <Route
+                                path="/product/:id"
+                                element={<ProductDetails />}
+                            />
+                            <Route path="/CartPage" element={<CartPage />} />
+                            <Route path="/Checkout" element={<Checkout />} />
+                            <Route path="/About" element={<About />} />
+                            {/* //////////////////////////Admin/////////////////////// */}
+                            <Route
+                                path="/dashboard"
+                                element={<Dashboard funcNav={setShowNav} />}
+                            >
+                                <Route index element={<DashMain />} />
+                                <Route
+                                    path="DashProducts"
+                                    element={<DashProducts />}
+                                />
+                                <Route path="DashMain" element={<DashMain />} />
+                            </Route>
+                            {/* <Route path="/addProduct" element={<AddProduct />} /> */}
+                            <Route path="*" element={<NoTFound />} />
+                        </Routes>
+                    </Container>
+                </main>
+                {showNav && (
+                    <footer>
+                        <BackTop />
+                        <Footer />
+                    </footer>
+                )}
+            </div>
+        </Router>
+    );
 }
 export default App;
