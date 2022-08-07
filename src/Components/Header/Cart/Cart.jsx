14dd9b614a17db9a7bd56111c6babb6858696d56
enas sello <enas.awad169@gmail.com>
2022-08-07 20:27:05 +0200

    Search

--- src/Components/Header/Cart/Cart.jsx
+++ src/Components/Header/Cart/Cart.jsx
@@ -1,19 +1,21 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { FiShoppingCart } from 'react-icons/fi';
 
 export default function Cart() {
-    const cart = useSelector((state) => state.cart);
-    const { cartItems } = cart;
-    return (
-        <>
-            <Link to="/CartPage" style={{ cursor: "pointer" }}>
-                <i className="fa-solid fa-solid fa-basket-shopping fs-2 header-cart text-white">
-                    <span className="cart-count ms-sm-1 me-3 font-monospace">
-                        {cartItems.length}
-                    </span>
-                </i>
-            </Link>
-        </>
-    );
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+  return (
+    <>
+      <Link to="/CartPage" style={{ cursor: 'pointer' }}>
+        <div className="cart">
+          <FiShoppingCart className="fa-solid fs-3 text-white" />
+          <span className="cart-count ms-sm-1 mt-1 me-3 font-monospace">
+            {cartItems.length}
+          </span>
+        </div>
+      </Link>
+    </>
+  );
 }
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "./Header.css";
 import { useSelector, useDispatch } from "react-redux";
 import { userSliceActions } from "../../Redux/userSlice.js";
 import logo from "../../assets/amazon_logo.png";
