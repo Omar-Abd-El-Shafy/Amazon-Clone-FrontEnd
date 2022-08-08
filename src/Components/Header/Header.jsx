f8b9bd2f644d776621ff9a92e45f1f62cb42eedc
Diaa Ahmed <diaa.aboelela@gmail.com>
2022-08-08 14:45:53 +0200

    yesterday's-push-again-ya-Enaaas

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -7,40 +7,45 @@ import Cart from "./Cart/Cart";
 import LogInButton from "./LoginButton/LoginButton";
 import { Link } from "react-router-dom";
 import Sidebar from "../SideBar/SideBar";
+import { MdAdminPanelSettings } from "react-icons/md";
 
 function Header() {
-  const loggedInUser = useSelector((state) => state.user.loggedInUser);
-  const dispatch = useDispatch();
+    const loggedInUser = useSelector((state) => state.user.loggedInUser);
+    const dispatch = useDispatch();
 
-  return (
-    <div className="">
-      <div className="Header fixed-top d-flex align-items-center justify-content-between">
-      <Sidebar/>
-        <Link to="/" style={{ cursor: "pointer" }}>
-          <img src={logo} alt="logo" className="logo" />
-        </Link>
-        <Search />
-        {loggedInUser?.userName ? (
-          <>
-            <span style={{ color: "white" }}>{loggedInUser.userName}</span>
-            <input
-              type="button"
-              onClick={() => {
-                dispatch(userSliceActions.logout());
-              }}
-              value="logout"
-            />
-          </>
-        ) : (
-          <>
-            <LogInButton />
-          </>
-        )}
-
-        <Cart />
-      </div>
-    </div>
-  );
+    return (
+        <div className="">
+            <div className="Header fixed-top d-flex align-items-center justify-content-between">
+                <Sidebar />
+                <Link to="/" style={{ cursor: "pointer" }}>
+                    <img src={logo} alt="logo" className="logo" />
+                </Link>
+                <Search />
+                {loggedInUser?.userName ? (
+                    <>
+                        <span style={{ color: "white" }}>
+                            {loggedInUser.userName}
+                        </span>
+                        <input
+                            type="button"
+                            onClick={() => {
+                                dispatch(userSliceActions.logout());
+                            }}
+                            value="logout"
+                        />
+                    </>
+                ) : (
+                    <>
+                        <LogInButton />
+                    </>
+                )}
+                <Link to="/dashboard">
+                    <MdAdminPanelSettings className="text-white admin-icon" />
+                </Link>
+                <Cart />
+            </div>
+        </div>
+    );
 }
 
 export default Header;
