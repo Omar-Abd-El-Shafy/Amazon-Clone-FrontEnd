9a5369afc9d884c50c8ca22fee0105fc64c6c720
Omar-Abd-El-Shafy <omarabdelshafy1010@gmail.com>
2022-08-11 14:11:08 +0200

    working on categoryForm

--- src/Components/Dashboard/DashProducts.js
+++ src/Components/Dashboard/DashProducts.js
@@ -1,12 +1,16 @@
 import React from "react";
 import AddProduct from "./AddProductsButton/AddProductsButton";
-
+import AddCategory from "./AddCategory/AddCategoryForm";
 export default function DashProducts() {
-    return (
-        <>
-            <div className="row ms-auto" style={{ width: "150px" }}>
-                <AddProduct />
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="row ms-auto" style={{ width: "150px" }}>
+        <AddProduct />
+      </div>
+
+      <div className="row ms-auto" style={{ width: "150px" }}>
+        <AddCategory />
+      </div>
+    </>
+  );
 }
