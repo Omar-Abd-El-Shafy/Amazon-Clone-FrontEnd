6ba276278a7eda853a5861a3c4683af9c743b119
Diaa Ahmed <diaa.aboelela@gmail.com>
2022-08-15 00:53:15 +0200

    filter-style

--- src/pages/SearchResults.jsx
+++ src/pages/SearchResults.jsx
@@ -7,6 +7,8 @@ import Row from "react-bootstrap/Row";
 import Dropdown from "react-bootstrap/Dropdown";
 import DropdownButton from "react-bootstrap/DropdownButton";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
+import { Link } from "react-router-dom";
+import Rating from "../Components/Rating/Rating";
 
 export default function SearchResults() {
     let param = useParams();
@@ -60,17 +62,39 @@ export default function SearchResults() {
                     products.map((product) => {
                         return (
                             <Col key={product._id}>
-                                <Card className="h-100">
-                                    <Card.Img
-                                        variant="top"
-                                        className="img-fluid w-75 h-100 m-auto searchImg"
-                                        src={product.image_path[0]}
-                                    />
-                                    <hr />
-                                    <Card.Body>
-                                        <Card.Title>{product.name}</Card.Title>
-                                        <Card.Text>{product.rating}</Card.Text>
-                                        <Card.Text>{product.price} $</Card.Text>
+                                <Card className="h-100 shadow border-0">
+                                    <Link to={`product/one/${product._id}`}>
+                                        <Card.Img
+                                            variant="top"
+                                            className="img-fluid w-100 m-auto searchImg"
+                                            src={product.image_path[0]}
+                                        />
+                                    </Link>
+                                    <Card.Body
+                                        style={{
+                                            display: "flex",
+                                            flexDirection: "column",
+                                            alignItems: "center",
+                                            justifyContent: "space-between",
+                                            textAlign: "center",
+                                        }}
+                                    >
+                                        <Link to={`/product/${product._id}`}>
+                                            <Card.Title>
+                                                {product.name}
+                                            </Card.Title>
+                                        </Link>
+                                        <Card.Text>
+                                            Rate: {product.rating}
+                                        </Card.Text>
+                                        <Card.Text
+                                            style={{
+                                                fontSize: "28px",
+                                                fontWeight: "400",
+                                            }}
+                                        >
+                                            {product.price} $
+                                        </Card.Text>
                                     </Card.Body>
                                 </Card>
                             </Col>
@@ -79,6 +103,65 @@ export default function SearchResults() {
                 ) : (
                     <h1 className="w-50 m-auto">{error}</h1>
                 )}
+                {/* {products && !error ? (
+                    products.map((product) => {
+                        return (
+                            <Card
+                                className="shadow border-0"
+                                // style={{ height: "100%" }}
+                                key={product._id}
+                            >
+                                <Link to={`/product/${product.id}`}>
+                                    <img
+                                        className="card-img-top"
+                                        src={product.image_path[0]}
+                                        alt={product.brand}
+                                    />
+                                </Link>
+                                <Card.Body
+                                    style={{
+                                        display: "flex",
+                                        flexDirection: "column",
+                                        alignItems: "center",
+                                        justifyContent: "space-between",
+                                        textAlign: "center",
+                                    }}
+                                >
+                                    <Link to={`/product/${product.id}`}>
+                                        <Card.Title>{product.name}</Card.Title>
+                                    </Link>
+                                    <Rating
+                                        rating={product.rating.rating}
+                                        Reviews={product.rating.count}
+                                    />
+                                    <Card.Text>
+                                        Rate: {product.rating}
+                                    </Card.Text>
+                                    <Card.Text
+                                        style={{
+                                            fontSize: "28px",
+                                            fontWeight: "400",
+                                        }}
+                                    >
+                                        {product.price} $
+                                    </Card.Text>
+                                    <Button
+                      className="bg-warning text-dark mt-auto shadow-sm"
+                      style={{
+                        border: 'none',
+                        fontSize: '20px',
+                      }}
+                      // onClick={handleAddToCart}
+                    >
+                      Add to Cart
+                    </Button>
+                                </Card.Body>
+                            </Card>
+                        );
+                    })
+                ) : (
+                    <h1 className="w-50 m-auto">{error}</h1>
+                )} */}
             </Row>
         </>
     );
