2c5d6827f66d3e06407782f1c104570660fc5580
muhammadabdalla <muhammadabdalla51@gmail.com>
2022-08-16 01:44:58 +0200

    add category

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,26 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import DashSidebar from "../Components/Dashboard/DashSidebar";
 import { Col, Row } from "react-bootstrap";
 
 const Users = (props) => {
+  useEffect(() => {
     props.funcNav(false);
-    return (
-        <div className="container-fluid">
-            <div className="row flex no-wrap">
-                <div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0">
-                    <Row>
-                        <Col xs={2} id="sidebar-wrapper">
-                            <DashSidebar />
-                        </Col>
-                    </Row>
-                </div>
-                <div className="col py-3">
-                    <Outlet />
-                </div>
-            </div>
+  });
+
+  return (
+    <div className="container-fluid">
+      <div className="row flex no-wrap">
+        <div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0">
+          <Row>
+            <Col xs={2} id="sidebar-wrapper">
+              <DashSidebar />
+            </Col>
+          </Row>
         </div>
-    );
+        <div className="col py-3">
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Users;
