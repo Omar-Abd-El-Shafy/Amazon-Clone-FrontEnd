03a31bcee0ea91f4ef393bbe83e6d29821dfad82
muhammadabdalla <muhammadabdalla51@gmail.com>
2022-08-16 02:36:42 +0200

    upating proplems

--- src/Components/user/UpdateName.jsx
+++ src/Components/user/UpdateName.jsx
@@ -8,88 +8,88 @@ import { Helmet } from "react-helmet-async";
 import { Button, Container, Form, InputGroup, Row } from "react-bootstrap";
 // import axios from "axios";
 import { Link } from "react-router-dom";
-export let token;
+//export let token;
 const UpdateName = () => {
-    token = JSON.parse(localStorage.getItem("userData"))?.token;
-    // const user = useSelector((state) => state.user.loggedInUser.user.name);
-    // console.log(token);
-    // props.funcNav(false);
-    const dispatch = useDispatch();
+  //token = JSON.parse(localStorage.getItem("userData"))?.token;
+  const token = useSelector((state) => state.user.loggedInUser?.token);
+  // console.log(token);
+  // props.funcNav(false);
+  const dispatch = useDispatch();
 
-    const [name, setName] = useState("");
-    // const token = useSelector((state) => state.user.loggedInUser.token);
-    // const user = useSelector((state) => state.user.loggedInUser.user.name);
-    // console.log(user);
-    // console.log(name);
-    // console.log(token);
-    // const loggedInUser = useSelector((state) => state.user.loggedInUser);
-    // const userName = useSelector((state) => state.user.loggedInUser.user.name);
+  const [name, setName] = useState("");
+  // const token = useSelector((state) => state.user.loggedInUser.token);
+  // const user = useSelector((state) => state.user.loggedInUser.user.name);
+  // console.log(user);
+  // console.log(name);
+  // console.log(token);
+  // const loggedInUser = useSelector((state) => state.user.loggedInUser);
+  // const userName = useSelector((state) => state.user.loggedInUser.user.name);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        // axios
-        //     .put(
-        //         "https://amazon-clone-deploy.herokuapp.com/user",
-        //         {
-        //             name: name,
-        //         },
-        //         {
-        //             headers: {
-        //                 "Content-Type": "application/json",
-        //                 "x-access-token": `${token}`,
-        //             },
-        //         }
-        //     )
-        //     .then((response) => {
-        //         console.log(response.data.user.name);
-        //         dispatch(userSliceActions.updateName({ name: response.data.user.name }));
-        //     })
-        //     .catch((error) => {
-        //         console.log(error);
-        //     });
-        dispatch(userSliceActions.updateUser({ name: name }));
-    };
-    return (
-        <Container style={{ maxWidth: "600px" }}>
-            <Row>
-                <Link to={"/profile"}>
-                    <h1>
-                        back to your account {"  "}
-                        <RiArrowGoBackFill />
-                    </h1>
-                </Link>
-                <hr />
-            </Row>
-            <Form>
-                <Helmet>
-                    <title>Edit profile information </title>
-                </Helmet>
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // axios
+    //     .put(
+    //         "https://amazon-clone-deploy.herokuapp.com/user",
+    //         {
+    //             name: name,
+    //         },
+    //         {
+    //             headers: {
+    //                 "Content-Type": "application/json",
+    //                 "x-access-token": `${token}`,
+    //             },
+    //         }
+    //     )
+    //     .then((response) => {
+    //         console.log(response.data.user.name);
+    //         dispatch(userSliceActions.updateName({ name: response.data.user.name }));
+    //     })
+    //     .catch((error) => {
+    //         console.log(error);
+    //     });
+    dispatch(userSliceActions.updateUser({ name, token }));
+  };
+  return (
+    <Container style={{ maxWidth: "600px" }}>
+      <Row>
+        <Link to={"/profile"}>
+          <h1>
+            back to your account {"  "}
+            <RiArrowGoBackFill />
+          </h1>
+        </Link>
+        <hr />
+      </Row>
+      <Form>
+        <Helmet>
+          <title>Edit profile information </title>
+        </Helmet>
 
-                <h4 className="text-dark"> Change your name</h4>
-                <Form.Text className="text-muted">
-                    If you want to change the name associated with your Amazon
-                    customer account, you may do so below. Be sure to click the
-                    Save Changes button when you are done.
-                </Form.Text>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>user Name</Form.Label>
-                    <Form.Control
-                        type="text"
-                        // placeholder={user}
-                        onChange={(e) => setName(e.target.value)}
-                    />
-                    <Button
-                        onClick={handleSubmit}
-                        className="mt-2 "
-                        variant="warning"
-                        type="submit"
-                    >
-                        Save changes
-                    </Button>
-                </Form.Group>
-            </Form>
-        </Container>
-    );
+        <h4 className="text-dark"> Change your name</h4>
+        <Form.Text className="text-muted">
+          If you want to change the name associated with your Amazon customer
+          account, you may do so below. Be sure to click the Save Changes button
+          when you are done.
+        </Form.Text>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>user Name</Form.Label>
+          <Form.Control
+            type="text"
+            // placeholder={user}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <Button
+            onClick={handleSubmit}
+            className="mt-2 "
+            variant="warning"
+            type="submit"
+          >
+            Save changes
+          </Button>
+        </Form.Group>
+      </Form>
+    </Container>
+  );
 };
 
 export default UpdateName;
--- src/Redux/userSlice.js
+++ src/Redux/userSlice.js
@@ -1,116 +1,119 @@
 import axios from "axios";
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import token from "../Components/user/UpdateName";
+//import token from "../Components/user/UpdateName";
 const initialState = {
-    loading: false,
-    loggedInUser: null,
-    error: "",
+  loading: false,
+  loggedInUser: null,
+  error: "",
 };
 // Generates pending, fulfilled and rejected action types
 // "https://amazon-clone-deploy.herokuapp.com/user/signup",
 
 export const login = createAsyncThunk("user/login", async (userData) => {
-    const response = await axios.post(
-        "https://amazon-clone-deploy.herokuapp.com/user/login",
-        userData
-    );
-    console.log(response.data);
-    return response.data;
+  const response = await axios.post(
+    "https://amazon-clone-deploy.herokuapp.com/user/login",
+    userData
+  );
+  console.log(response.data);
+  return response.data;
 });
 ///////////////////////////////////////
 // "https://amazon-clone-deploy.herokuapp.com/user/register",
 
 export const register = createAsyncThunk("user/register", async (userData) => {
-    const response = await axios.post(
-        "https://amazon-clone-deploy.herokuapp.com/user/register",
-        userData
-    );
-    return response.data;
+  const response = await axios.post(
+    "https://amazon-clone-deploy.herokuapp.com/user/register",
+    userData
+  );
+  return response.data;
 });
 
 // update user
 export const updateUser = createAsyncThunk("user", async (userData) => {
-    const storedData = JSON.parse(localStorage.getItem("userData"));
-    console.log(userData);
-    const response = await axios
-        .put("https://amazon-clone-deploy.herokuapp.com/user", userData, {
-            headers: {
-                "Content-Type": "application/json",
-                "x-access-token": `${token}`,
-            },
-        })
-        .then((response) => {
-            console.log(response.data.user.name);
-        })
-        .catch((error) => {
-            console.log(error);
-        });
-    return response.data;
+  // const storedData = JSON.parse(localStorage.getItem("userData"));
+  console.log(userData);
+  const response = await axios.put(
+    "https://amazon-clone-deploy.herokuapp.com/user",
+    { name: userData.name },
+    {
+      headers: {
+        // "Content-Type": "application/json",
+        "x-access-token": `${userData.token}`,
+      },
+    }
+  );
+  // .then((response) => {
+  //   console.log(response.data.user.name);
+  // })
+  // .catch((error) => {
+  //   console.log(error);
+  // });
+  return response.data;
 });
 const userSlice = createSlice({
-    name: "user",
-    initialState,
-    reducers: {
-        logout(state, action) {
-            localStorage.removeItem("userData");
-            state.loggedInUser = null;
-        },
-        setUser(state, action) {
-            state.loggedInUser = action.payload;
-        },
+  name: "user",
+  initialState,
+  reducers: {
+    logout(state, action) {
+      localStorage.removeItem("userData");
+      state.loggedInUser = null;
     },
-    extraReducers: (builder) => {
-        builder.addCase(login.pending, (state) => {
-            state.loading = true;
-        });
-        builder.addCase(login.fulfilled, (state, action) => {
-            state.loading = false;
-            state.loggedInUser = action.payload;
-            state.error = "";
-        });
-        builder.addCase(login.rejected, (state, action) => {
-            state.loading = false;
-            state.loggedInUser = null;
-            state.error = action.error.message;
-        });
+    setUser(state, action) {
+      state.loggedInUser = action.payload;
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(login.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(login.fulfilled, (state, action) => {
+      state.loading = false;
+      state.loggedInUser = action.payload;
+      state.error = "";
+    });
+    builder.addCase(login.rejected, (state, action) => {
+      state.loading = false;
+      state.loggedInUser = null;
+      state.error = action.error.message;
+    });
 
-        /////////////////////////////////////////
-        builder.addCase(register.pending, (state) => {
-            state.loading = true;
-        });
-        builder.addCase(register.fulfilled, (state, action) => {
-            state.loading = false;
-            state.loggedInUser = action.payload;
-            state.error = "";
-        });
-        builder.addCase(register.rejected, (state, action) => {
-            state.loading = false;
-            state.loggedInUser = null;
-            state.error = action.error.message;
-        });
-        //////////////
-        builder.addCase(updateUser.pending, (state) => {
-            state.loading = true;
-        });
-        builder.addCase(updateUser.fulfilled, (state, action) => {
-            console.log(action.payload);
-            state.loading = false;
-            state.loggedInUser = action.payload;
-            state.error = "";
-        });
-        builder.addCase(updateUser.rejected, (state, action) => {
-            state.loading = false;
-            state.loggedInUser = null;
-            state.error = action.error.message;
-        });
-    },
+    /////////////////////////////////////////
+    builder.addCase(register.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(register.fulfilled, (state, action) => {
+      state.loading = false;
+      state.loggedInUser = action.payload;
+      state.error = "";
+    });
+    builder.addCase(register.rejected, (state, action) => {
+      state.loading = false;
+      state.loggedInUser = null;
+      state.error = action.error.message;
+    });
+    //////////////
+    builder.addCase(updateUser.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(updateUser.fulfilled, (state, action) => {
+      console.log(action.payload);
+      state.loading = false;
+      state.loggedInUser.user.name = action.payload.user.name;
+      state.error = "";
+    });
+    builder.addCase(updateUser.rejected, (state, action) => {
+      state.loading = false;
+      state.loggedInUser = null;
+      state.error = action.error.message;
+    });
+  },
 });
 
 export const userSliceActions = {
-    ...userSlice.actions,
-    login,
-    register,
-    updateUser,
+  ...userSlice.actions,
+  login,
+  register,
+  updateUser,
 };
 
 export default userSlice.reducer;
