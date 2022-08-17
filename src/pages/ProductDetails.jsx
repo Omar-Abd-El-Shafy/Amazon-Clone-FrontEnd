da55c42a7f0c71479b49de065a7ec6715339d937
Diaa Ahmed <diaa.aboelela@gmail.com>
2022-08-18 00:59:33 +0200

    sideBar

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {
-  useDeleteProductMutation,
-  useGetSingleProdactQuery,
+    useDeleteProductMutation,
+    useGetSingleProdactQuery,
 } from "../Redux/Api";
 import { useParams, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
@@ -24,201 +24,212 @@ import { FaHeart, FaRegHeart } from "react-icons/fa";
 import { addToFavourites, removeFromFavourites } from "../Redux/favouriteSlice";
 
 function ProductDetails() {
-  const params = useParams();
-  const { id } = params;
-  const loggedInUser = useSelector((state) => state.user?.loggedInUser);
-  // console.log(loggedInUser.user.role);
-  const [Selectedimg, SetSelectedImg] = useState();
-  const navigate = useNavigate();
-  const {
-    data: product,
-    error,
-    isLoading: loading,
-  } = useGetSingleProdactQuery(id);
-  const Dispatch = useDispatch();
-  const handleAddToCart = (product) => {
-    Dispatch(addToCart(product));
-  };
-  console.log(product);
-  // create deleteProduct function
-  //   const deleteProduct = () => {
-  //       console.log("deleteProduct");
-  // };
-  const [deleteProduct, { isError }] = useDeleteProductMutation();
+    const params = useParams();
+    const { id } = params;
+    const loggedInUser = useSelector((state) => state.user?.loggedInUser);
+    // console.log(loggedInUser.user.role);
+    const [Selectedimg, SetSelectedImg] = useState();
+    const navigate = useNavigate();
+    const {
+        data: product,
+        error,
+        isLoading: loading,
+    } = useGetSingleProdactQuery(id);
+    const Dispatch = useDispatch();
+    const handleAddToCart = (product) => {
+        Dispatch(addToCart(product));
+    };
+    console.log(product);
+    // create deleteProduct function
+    //   const deleteProduct = () => {
+    //       console.log("deleteProduct");
+    // };
+    const [deleteProduct, { isError }] = useDeleteProductMutation();
 
-  const [like, setLike] = useState(false);
-  const [saved, setSaved] = useState(false);
-  const userinfo = useSelector((state) => state.user.loggedInUser);
-  const saveShow = (product) => {
-    if (userinfo) {
-      setLike(!like);
-      setSaved(product);
-    } else {
-      navigate("/login");
+    const [like, setLike] = useState(false);
+    const [saved, setSaved] = useState(false);
+    const userinfo = useSelector((state) => state.user.loggedInUser);
+    const saveShow = (product) => {
+        if (userinfo) {
+            setLike(!like);
+            setSaved(product);
+        } else {
+            navigate("/login");
+        }
+    };
+    if (!product) {
+        return null;
     }
-  };
 
-  if (!product) {
-    return null;
-  }
-
-  return loading ? (
-    <div>
-      <Loading />
-    </div>
-  ) : error ? (
-    <Error variant="danger">{error.message}</Error>
-  ) : (
-    <div className="text-capitalize">
-      <Row>
-        {" "}
-        <h5
-          onClick={() => {
-            saveShow(product);
-          }}
-        >
-          {like ? (
-            <FaHeart
-              className="text-danger"
-              onClick={() => Dispatch(removeFromFavourites(product))}
-            />
-          ) : (
-            <FaRegHeart
-              className=" text-black"
-              onClick={() => Dispatch(addToFavourites(product))}
-            />
-          )}
-        </h5>
-        <Col md={5}>
-          <img
-            style={{ maxWidth: "100%" }}
-            src={Selectedimg || product.image_path[0]}
-            alt={product.name}
-            className="w-75"
-          ></img>
-        </Col>
-        <Col md={3}>
-          <ListGroup variant="flush">
-            <ListGroup.Item>
-              <Helmet>
-                <title>{product.name}</title>
-              </Helmet>
-              <h1 className="text-secondary">{product.name}</h1>
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <Row xs={1} md={2} className="">
-                {product.image_path.map((x) => (
-                  <Col key={x}>
-                    <Card className="border-0">
-                      <Button
-                        className="p-0"
-                        type="button"
-                        variant="light"
-                        onClick={() => {
-                          SetSelectedImg(x);
-                        }}
-                      >
-                        <Card.Img
-                          variant="topp"
-                          src={x}
-                          alt="product"
-                          style={{
-                            width: "50px",
-                            maxWidth: "50px",
-                          }}
-                        ></Card.Img>
-                      </Button>
-                    </Card>
-                  </Col>
-                ))}
-              </Row>
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <Rating rating={product.rating} Reviews={product.rating} />
-            </ListGroup.Item>
-            <ListGroup.Item className="text-danger fw-bolder">
-              price :{product.price} EGP
-            </ListGroup.Item>
-            <ListGroup.Item>
-              products Description :<p>{product.description}</p>
-            </ListGroup.Item>
-          </ListGroup>
-        </Col>
-        <Col md={3}>
-          <Card>
-            <Card.Body>
-              <ListGroup variant="flush">
-                <Row className="align-items-center">
-                  <Col>status:</Col>
-                  <Col>
-                    {product.stock > 0 ? (
-                      <Badge className=" text-success text-center m-1 p-1 ">
-                        in stock
-                      </Badge>
+    return loading ? (
+        <div>
+            <Loading />
+        </div>
+    ) : error ? (
+        <Error variant="danger">{error.message}</Error>
+    ) : (
+        <div className="text-capitalize">
+            <Row>
+                {" "}
+                <h5
+                    onClick={() => {
+                        saveShow(product);
+                    }}
+                >
+                    {like ? (
+                        <FaHeart
+                            className="text-danger"
+                            onClick={() =>
+                                Dispatch(removeFromFavourites(product))
+                            }
+                        />
                     ) : (
-                      <Badge className=" text-danger text-center m-1 p-1 ">
-                        out of stock
-                      </Badge>
+                        <FaRegHeart
+                            className=" text-black"
+                            onClick={() => Dispatch(addToFavourites(product))}
+                        />
                     )}
-                  </Col>
-                  <ListGroup.Item className="border-0">
-                    price:
-                    <span className="text-danger fw-bolder">
-                      {product.price} EGP
-                    </span>
-                  </ListGroup.Item>
-                </Row>
-              </ListGroup>
-              <ListGroup>
-                <div className="d-grid">
-                  {product.stock > 0 ? (
-                    <Button
-                      className="rounded-pill "
-                      variant="warning"
-                      onClick={() => handleAddToCart(product)}
-                    >
-                      Add to Cart
-                    </Button>
-                  ) : (
-                    <Button variant="secondary">out of stock</Button>
-                  )}
-                </div>
-              </ListGroup>
-            </Card.Body>
-          </Card>
-          <>
-            {loggedInUser?.user.role === true ? (
-              <Button
-                className="rounded-pill mt-3 d-block ms-auto"
-                variant="danger"
-                onClick={() => {
-                  console.log(product._id);
-                  deleteProduct({
-                    token: loggedInUser.token,
-                    id: product._id,
-                  });
-                  if (!isError) {
-                    toast.success(`Product Deleted Successfully`, {
-                      position: "bottom-left",
-                      autoClose: 3000,
-                      hideProgressBar: false,
-                      closeOnClick: true,
-                      pauseOnHover: true,
-                      draggable: true,
-                      progress: undefined,
-                    });
-                    navigate("/");
-                  }
-                }}
-              >
-                <IoTrashOutline />
-              </Button>
-            ) : null}
-          </>
-        </Col>
-      </Row>
-    </div>
-  );
+                </h5>
+                <Col md={5}>
+                    <img
+                        style={{ maxWidth: "100%" }}
+                        src={Selectedimg || product.image_path[0]}
+                        alt={product.name}
+                        className="w-75"
+                    ></img>
+                </Col>
+                <Col md={3}>
+                    <ListGroup variant="flush">
+                        <ListGroup.Item>
+                            <Helmet>
+                                <title>{product.name}</title>
+                            </Helmet>
+                            <h1 className="text-secondary">{product.name}</h1>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <Row xs={1} md={2} className="">
+                                {product.image_path.map((x) => (
+                                    <Col key={x}>
+                                        <Card className="border-0">
+                                            <Button
+                                                className="p-0"
+                                                type="button"
+                                                variant="light"
+                                                onClick={() => {
+                                                    SetSelectedImg(x);
+                                                }}
+                                            >
+                                                <Card.Img
+                                                    variant="topp"
+                                                    src={x}
+                                                    alt="product"
+                                                    style={{
+                                                        width: "50px",
+                                                        maxWidth: "50px",
+                                                    }}
+                                                ></Card.Img>
+                                            </Button>
+                                        </Card>
+                                    </Col>
+                                ))}
+                            </Row>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <Rating
+                                rating={product.rating}
+                                Reviews={product.rating}
+                            />
+                        </ListGroup.Item>
+                        <ListGroup.Item className="text-danger fw-bolder">
+                            price :{product.price} EGP
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            products Description :<p>{product.description}</p>
+                        </ListGroup.Item>
+                    </ListGroup>
+                </Col>
+                <Col md={3}>
+                    <Card>
+                        <Card.Body>
+                            <ListGroup variant="flush">
+                                <Row className="align-items-center">
+                                    <Col>status:</Col>
+                                    <Col>
+                                        {product.stock > 0 ? (
+                                            <Badge className=" text-success text-center m-1 p-1 ">
+                                                in stock
+                                            </Badge>
+                                        ) : (
+                                            <Badge className=" text-danger text-center m-1 p-1 ">
+                                                out of stock
+                                            </Badge>
+                                        )}
+                                    </Col>
+                                    <ListGroup.Item className="border-0">
+                                        price:
+                                        <span className="text-danger fw-bolder">
+                                            {product.price} EGP
+                                        </span>
+                                    </ListGroup.Item>
+                                </Row>
+                            </ListGroup>
+                            <ListGroup>
+                                <div className="d-grid">
+                                    {product.stock > 0 ? (
+                                        <Button
+                                            className="rounded-pill "
+                                            variant="warning"
+                                            onClick={() =>
+                                                handleAddToCart(product)
+                                            }
+                                        >
+                                            Add to Cart
+                                        </Button>
+                                    ) : (
+                                        <Button variant="secondary">
+                                            out of stock
+                                        </Button>
+                                    )}
+                                </div>
+                            </ListGroup>
+                        </Card.Body>
+                    </Card>
+                    <>
+                        {loggedInUser?.user.role === true ? (
+                            <Button
+                                className="rounded-pill mt-3 d-block ms-auto"
+                                variant="danger"
+                                onClick={() => {
+                                    console.log(product._id);
+                                    deleteProduct({
+                                        token: loggedInUser.token,
+                                        id: product._id,
+                                    });
+                                    if (!isError) {
+                                        toast.success(
+                                            `Product Deleted Successfully`,
+                                            {
+                                                position: "bottom-left",
+                                                autoClose: 3000,
+                                                hideProgressBar: false,
+                                                closeOnClick: true,
+                                                pauseOnHover: true,
+                                                draggable: true,
+                                                progress: undefined,
+                                            }
+                                        );
+                                        navigate("/");
+                                    }
+                                }}
+                            >
+                                <IoTrashOutline />
+                            </Button>
+                        ) : null}
+                    </>
+                </Col>
+            </Row>
+        </div>
+    );
 }
 
 export default ProductDetails;
