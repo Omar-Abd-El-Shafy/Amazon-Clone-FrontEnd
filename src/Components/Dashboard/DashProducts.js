015db50364be1dd13cd4288e829dc842c602ed2e
Radwa Ayman <72885354+radwaymn@users.noreply.github.com>
2022-08-18 02:43:20 +0200

    dashboard: update category

--- src/Components/Dashboard/DashProducts.js
+++ src/Components/Dashboard/DashProducts.js
@@ -2,6 +2,7 @@ import React from "react";
 import AddProduct from "./AddProductsButton/AddProductsButton";
 import AddCategory from "./AddCategory/AddCategoryForm";
 import DeleteCategory from "./DeleteCategory/DeleteCategoryForm";
+import UpdateCategory from "./UpdateCategory/UpdateCategoryForm";
 export default function DashProducts() {
   return (
     <>
@@ -11,6 +12,10 @@ export default function DashProducts() {
 
       <div className="row ms-auto" style={{ width: "150px" }}>
         <AddCategory />
+      </div>
+
+      <div className="row ms-auto" style={{ width: "150px" }}>
+        <UpdateCategory />
       </div>
 
       <div className="row ms-auto" style={{ width: "150px" }}>
