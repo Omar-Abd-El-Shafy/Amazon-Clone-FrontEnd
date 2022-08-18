552029ae6c8fc754ce289eeb624564d07d0506ad
enas sello <enas.awad169@gmail.com>
2022-08-18 17:00:31 +0200

    addaddress

--- src/pages/test.js
+++ src/pages/test.js
@@ -1,10 +1,8 @@
-import React from 'react';
-import { useAddReviewMutation } from '../../Redux/Api';
+const [phone, setPhone] = useState(Shipping.phone || ' ');
+const [street, setStreet] = useState(Shipping.phone || ' ');
+const [building, setBuilding] = useState(Shipping.phone || ' ');
+const [city, setCity] = useState(Shipping.city || ' ');
+const [state, setState] = useState(Shipping.city || ' ');
+const [zipCode, setZipCode] = useState(Shipping.city || ' ');
 
-const AddReview = () => {
-  const { data, isLoading, isError } = useAddReviewMutation();
-
-  return <div>AddReview</div>;
-};
-
-export default AddReview;
+// Example: `updatePost().unwrap().then(fulfilled => console.log(fulfilled)).catch(rejected => console.error(rejected))
