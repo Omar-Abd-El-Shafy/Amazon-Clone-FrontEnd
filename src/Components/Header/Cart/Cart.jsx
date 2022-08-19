09d127ad3001684ad50b18f7b9549c00d5c41333
Radwa Ayman <72885354+radwaymn@users.noreply.github.com>
2022-08-19 10:34:55 +0200

    use skip to avoid loggedInUser is null error

--- src/Components/Header/Cart/Cart.jsx
+++ src/Components/Header/Cart/Cart.jsx
@@ -5,21 +5,23 @@ import { RiShoppingCartLine } from "react-icons/ri";
 import { useGetUserCartQuery } from "../../../Redux/Api";
 
 export default function Cart() {
-    const cart = useSelector((state) => state.cart);
-    const { cartItems } = cart;
-    const loggedInUser = useSelector((state) => state.user?.loggedInUser);
-     const { data: cartData } = useGetUserCartQuery(loggedInUser.token);
-    
-    return (
-      <>
-        <Link to="/CartPage" style={{ cursor: 'pointer' }}>
-          <div className="cart">
-            <span className="cart-count ms-sm-1 mt-1 me-3 font-monospace">
-              {cartData.products.length}
-            </span>
-            <RiShoppingCartLine className="fa-solid fs-3 me-4 text-white cart-img" />
-          </div>
-        </Link>
-      </>
-    );
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+  const loggedInUser = useSelector((state) => state.user?.loggedInUser);
+  const { data: cartData } = useGetUserCartQuery(loggedInUser?.token, {
+    skip: loggedInUser ? false : true,
+  });
+
+  return (
+    <>
+      <Link to="/CartPage" style={{ cursor: "pointer" }}>
+        <div className="cart">
+          <span className="cart-count ms-sm-1 mt-1 me-3 font-monospace">
+            {cartData?.products.length}
+          </span>
+          <RiShoppingCartLine className="fa-solid fs-3 me-4 text-white cart-img" />
+        </div>
+      </Link>
+    </>
+  );
 }
