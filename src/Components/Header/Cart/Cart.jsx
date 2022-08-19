55aae6b7b6d9480d0f15d180372876aab2b3bb12
enas sello <enas.awad169@gmail.com>
2022-08-19 11:11:27 +0200

    Cart in header

--- src/Components/Header/Cart/Cart.jsx
+++ src/Components/Header/Cart/Cart.jsx
@@ -6,21 +6,20 @@ import { useGetUserCartQuery } from '../../../Redux/Api';
 
 export default function Cart() {
   const cart = useSelector((state) => state.cart);
-  const navigate = useNavigate();
-
   const { cartItems } = cart;
   const loggedInUser = useSelector((state) => state.user?.loggedInUser);
-  if (!loggedInUser) {
-    navigate('/login');
-  }
-  const { data: cartData } = useGetUserCartQuery(loggedInUser?.token);
+  const { data: cartData } = useGetUserCartQuery(loggedInUser?.token, {
+    skip: loggedInUser ? false : true,
+  });
+ 
+
   // console.log(cartData.products.length);
   return (
     <>
       <Link to="/CartPage" style={{ cursor: 'pointer' }}>
         <div className="cart">
           <span className="cart-count ms-sm-1 mt-1 me-3 font-monospace">
-            {!cartData ? <>{cartData?.products.length}</> : <>0</>}
+           {cartData?.products.length}
           </span>
           <RiShoppingCartLine className="fa-solid fs-3 me-4 text-white cart-img" />
         </div>
