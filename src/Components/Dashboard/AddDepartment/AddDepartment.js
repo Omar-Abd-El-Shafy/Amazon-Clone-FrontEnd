716d08132ec69f36f5978f1245648ed4f64bffe1
muhammadabdalla <muhammadabdalla51@gmail.com>
2022-08-20 02:43:19 +0200

    add dep

---
+++ src/Components/Dashboard/AddDepartment/AddDepartment.js
@@ -0,0 +1,115 @@
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as yup from "yup";
+import { toast } from "react-toastify";
+import { useSelector } from "react-redux";
+import { useAddDepartmentMutation } from "../../../Redux/Api";
+
+export default function AddDepartment() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const loggedInUser = useSelector((state) => state.user.loggedInUser);
+  // const { data: departments } = useGetdAlldepartmentQuery();
+  const [addDepartment, { isError }] = useAddDepartmentMutation();
+  const schema = yup.object().shape({
+    name: yup.string().required("Required Field"),
+  });
+
+  return (
+    <>
+      <Button variant="warning" onClick={handleShow} className="crudBtn">
+        Add department
+      </Button>
+
+      <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Add New Category</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Formik
+            enableReinitialize
+            initialValues={{
+              name: "",
+            }}
+            validationSchema={schema}
+            onSubmit={(values) => {
+              console.log(values);
+              addDepartment({
+                token: loggedInUser.token,
+                body: {
+                  name: values.name,
+                },
+              })
+                .unwrap()
+                .then((fulfilled) => {
+                  toast.success(`Category Added Successfully`, {
+                    position: "bottom-center",
+                    autoClose: 3000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                  });
+                })
+                .catch((rejected) => console.error(rejected.data));
+            }}
+          >
+            {({ setFieldValue }) => (
+              <Form id="btnId">
+                {/* department
+                                <div className="form-control">
+                                    <label htmlFor="department">
+                                        department
+                                    </label>
+                                    <Field name="department" as="select">
+                                        <option value="" disabled>
+                                            Select an option
+                                        </option>
+                                        {departments?.map((dept) => (
+                                            <option
+                                                key={dept._id}
+                                                value={dept._id}
+                                            >
+                                                {dept.name}
+                                            </option>
+                                        ))}
+                                    </Field>
+                                    <div className="ErrorMessageTxt">
+                                        <ErrorMessage name="department" />
+                                    </div>
+                                </div> */}
+                {/* name */}
+                <div className="form-control">
+                  <label htmlFor="name">Department name</label>
+                  <Field name="name" type="text" />
+                  <div className="ErrorMessageTxt">
+                    <ErrorMessage name="name" />
+                  </div>
+                </div>
+              </Form>
+            )}
+          </Formik>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="danger" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="warning" type="submit" form="btnId">
+            Submit
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
