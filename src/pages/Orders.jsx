0e724bf74340a25680a3e59c957b8ac14794ea88
enas sello <enas.awad169@gmail.com>
2022-08-21 01:40:07 +0200

    Orders

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Card, Col, Container, Row } from 'react-bootstrap';
+import { Button, Card, Col, Container, Row } from 'react-bootstrap';
 import { Helmet } from 'react-helmet-async';
 import { useSelector } from 'react-redux';
 import Error from '../Components/Error/Error';
 import Loading from '../Components/Loading/Loading';
 import { useGetAllOrderQuery } from '../Redux/Api';
-import Table from 'react-bootstrap/Table';
+// import TbTruckLoading from 'react-icons/tb';
 import { Link } from 'react-router-dom';
 
 const Orders = () => {
@@ -26,7 +26,7 @@ const Orders = () => {
       ) : (
         <>
           {data.map((item) => (
-            <Card key={item._id}>
+            <Card className="m-2" key={item._id}>
               <Row className="m-0  bg-secondary bg-opacity-10">
                 <Col md={8} className=" d-flex justify-content-between">
                   <Card.Text>
@@ -43,10 +43,48 @@ const Orders = () => {
                   deliveryAddress:<strong>{item.deliveryAddress.city}</strong>
                 </Col>
               </Row>
-              <Row className="m-0 p-1">Delivered {item.deliveryDate}</Row>
+              {/* <Row className="m-0 p-1">Delivered {item.deliveryDate}</Row> */}
               {item.products.map((x) => (
-                <Row className="m-0 p-1">
-                  <Card.Img style={{width:'50px'}} src={x.productBrief.image_path} />
+                <Row key={x.product_id} className="m-0 p-1">
+                  <Col md={1}>
+                    <Card.Img
+                      style={{ width: '50px' }}
+                      src={x.productBrief.image_path}
+                    />
+                  </Col>
+                  <Col md={6}>
+                    <Card.Text>{x.productBrief.name}</Card.Text>
+                    <Card.Text>price: {x.productBrief.price}EGP</Card.Text>
+                    <Link to={`/product/one/${x.productBrief.product_id}`}>
+                      <Button
+                        variant="warning"
+                        className="shadow-sm  bg-gradient bg-opacity-10  px-2 py-1  m-2"
+                      >
+                        <span>
+                          {/* <TbTruckLoading  className="text-dark"/> */}
+                          Buy it again
+                        </span>
+                      </Button>
+                    </Link>
+                  </Col>
+                  <Col md={5} className=" mt-3">
+                    <Link to={`/product/one/${x.productBrief.product_id}`}>
+                      <Button
+                        variant="light"
+                        className="shadow  bg-gradient bg-opacity-10  px-2 py-1  m-2"
+                      >
+                        Write a product review
+                      </Button>
+                    </Link>
+                    <Link to={'/'}>
+                      <Button
+                        variant="light"
+                        className="shadow bg-gradient bg-opacity-10  px-2 py-1  m-2"
+                      >
+                        back to Home
+                      </Button>
+                    </Link>
+                  </Col>
                 </Row>
               ))}
             </Card>
