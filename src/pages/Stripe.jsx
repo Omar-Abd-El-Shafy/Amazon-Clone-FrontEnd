37e7497bc69a9edb882b6a833807c2f7a087c58a
Omar-Abd-El-Shafy <omarabdelshafy1010@gmail.com>
2022-08-23 12:23:44 +0200

    pushing working code and fixing empty json

--- src/pages/Stripe.jsx
+++ src/pages/Stripe.jsx
@@ -1,50 +1,50 @@
-import React, { useState, useEffect } from 'react';
-import { loadStripe } from '@stripe/stripe-js';
-import { Elements } from '@stripe/react-stripe-js';
+import React, { useState, useEffect } from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
 
-import CheckoutForm from './CheckoutForm';
-import './Stripe.css';
-import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router';
-import { useLocation } from 'react-router-dom';
-import { Col, Container, Row } from 'react-bootstrap';
-import OrderSummery from '../Components/orders/OrderSummery';
+import CheckoutForm from "./CheckoutForm";
+import "./Stripe.css";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router";
+import { useLocation } from "react-router-dom";
+import { Col, Container, Row } from "react-bootstrap";
+import OrderSummery from "../Components/orders/OrderSummery";
 
 // Make sure to call loadStripe outside of a component’s render to avoid
 // recreating the Stripe object on every render.
 // This is your test publishable API key.
 const stripePromise = loadStripe(
-  'pk_test_51LTllPFwhSEkFDCITyyEeRWRWzutCNUpPLOVIjIxHxN9uExzSruNMu1Add2JKRRAf9OyQdcca00EQd70ccm35VBf00D0RNjgkw'
+  "pk_test_51LTllPFwhSEkFDCITyyEeRWRWzutCNUpPLOVIjIxHxN9uExzSruNMu1Add2JKRRAf9OyQdcca00EQd70ccm35VBf00D0RNjgkw"
 );
 
 export default function Stripe() {
-  const [clientSecret, setClientSecret] = useState('');
+  const [clientSecret, setClientSecret] = useState("");
 
   const location = useLocation();
   const loggedInUser = useSelector((state) => state.user.loggedInUser);
   const navigate = useNavigate();
   useEffect(() => {
     if (!loggedInUser) {
-      navigate('/login');
+      navigate("/login");
     }
   }, [loggedInUser, navigate]);
 
   useEffect(() => {
     // Create PaymentIntent as soon as the page loads
-    fetch('/payment/create-payment-intent', {
-      method: 'POST',
+    fetch("/payment/create-payment-intent", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
-        'x-access-token': `${loggedInUser.token}`,
+        "Content-Type": "application/json",
+        "x-access-token": `${loggedInUser.token}`,
       },
       body: JSON.stringify({ order_id: location.state.order_id }),
     })
-      .then((res) => res.json())
+      .then((res) => res.json({}))
       .then((data) => setClientSecret(data.clientSecret));
   }, [loggedInUser, location.state.order_id]);
 
   const appearance = {
-    theme: 'stripe',
+    theme: "stripe",
   };
   const options = {
     clientSecret,
